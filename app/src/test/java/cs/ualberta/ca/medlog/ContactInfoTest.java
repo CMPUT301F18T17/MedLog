04f1f3df7385c5c8f507bed8001b45008edebe85
temtamre <ttamre@ualberta.ca>
2018-11-02 02:28:43 -0600

    Finished all tests except RecordTest

--- app/src/test/java/cs/ualberta/ca/medlog/ContactInfoTest.java
+++ app/src/test/java/cs/ualberta/ca/medlog/ContactInfoTest.java
@@ -1,3 +1,11 @@
+/*
+ * Test class for the ContactInfo entity
+ *
+ * Authors: Thomas Roskewich, Tem Tamre
+ * Contact: roskewic@ualberta.ca, ttamre@ualberta.ca
+ * Created: October 30, 2018
+ */
+
 package cs.ualberta.ca.medlog;
 
 import org.junit.Test;
@@ -9,49 +17,63 @@ import static org.junit.Assert.assertEquals;
 public class ContactInfoTest {
 
     @Test
-    public void emailTests(){
+    public void testGetEmail(){
         ContactInfo ct = new ContactInfo("1234567890", "test@test.com");
         assertEquals(ct.getEmail(), "test@test.com");
+    }
+
+    @Test
+    public void testSetEmail() {
+        ContactInfo ct = new ContactInfo("1234567890", "test@test.com");
+        assertEquals(ct.getEmail(), "test@test.com");
+
         ct.setEmail("superTest@test.com");
         assertEquals(ct.getEmail(), "superTest@test.com");
     }
 
     @Test
-    public void phoneTests(){
-        ContactInfo ct = new ContactInfo("1234567890", "test@test.com");
-        assertEquals(ct.getPhoneNumber(), "1234567890");
-        ct.setPhoneNumber("9876543210");
-        assertEquals(ct.getPhoneNumber(), "9876543210");
-    }
-
-    @Test
-    public void invalidEmailTest(){
+    public void testInvalidEmail(){
         ContactInfo ct;
-        try{
+        try {
             ct = new ContactInfo("1234567890", "test");
-        }catch(Exception e){
+        } catch(Exception e) {
             ct = new ContactInfo("1234567890", "test@test.com");
         }
         assertEquals(ct.getEmail(), "test@test.com");
     }
 
     @Test
-    public void invalidPhoneTest(){
+    public void testGetPhoneNumber(){
+        ContactInfo ct = new ContactInfo("1234567890", "test@test.com");
+        assertEquals(ct.getPhoneNumber(), "1234567890");
+    }
+
+    @Test
+    public void testSetPhoneNumber() {
+        ContactInfo ct = new ContactInfo("1234567890", "test@test.com");
+        assertEquals(ct.getPhoneNumber(), "1234567890");
+
+        ct.setPhoneNumber("9876543210");
+        assertEquals(ct.getPhoneNumber(), "9876543210");
+    }
+
+    @Test
+    public void testInvalidPhoneNumber(){
         ContactInfo ct;
 
-        try{
+        try {
             ct = new ContactInfo("1234", "test");
-        }catch(Exception e){
+        } catch(Exception e) {
             ct = new ContactInfo("1234567890", "test@test.com");
         }
-        assertEquals(ct.getPhoneNumber(), "1234567890");
+        assertEquals("1234567890", ct.getPhoneNumber());
 
-        try{
+        try {
             ct = new ContactInfo("wow", "test");
-        }catch(Exception e){
+        } catch(Exception e) {
             ct = new ContactInfo("1234567890", "test@test.com");
         }
-        assertEquals(ct.getPhoneNumber(), "1234567890");
+        assertEquals("1234567890", ct.getPhoneNumber());
     }
 
 
