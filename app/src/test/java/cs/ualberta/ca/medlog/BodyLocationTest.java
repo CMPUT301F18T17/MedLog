4959917c1e017e248b6705082ef726a95f8c12fb
Thomas Roskewich <roskewic@ualberta.ca>
2018-11-01 18:10:32 -0600

    Updated tests to new model methods

--- app/src/test/java/cs/ualberta/ca/medlog/BodyLocationTest.java
+++ app/src/test/java/cs/ualberta/ca/medlog/BodyLocationTest.java
@@ -11,21 +11,21 @@ public class BodyLocationTest {
 
     @Test
     public void testBodyLocationPhoto(){
-        Photo p = new Photo(0);
+        Photo p = new Photo(0, null);
         BodyLocation bl = new BodyLocation(p, 10, 5);
         assertEquals(bl.getPhoto().getIdentifier(), p.getIdentifier());
     }
 
     @Test
     public void testBodyLocationX(){
-        Photo p = new Photo(0);
+        Photo p = new Photo(0, null);
         BodyLocation bl = new BodyLocation(p, 10, 5);
         assertEquals(bl.getX(), 10);
     }
 
     @Test
     public void testBodyLocationY(){
-        Photo p = new Photo(0);
+        Photo p = new Photo(0, null);
         BodyLocation bl = new BodyLocation(p, 10, 5);
         assertEquals(bl.getY(), 5);
     }
