c292df45b100e31373e0c40643fcee86b3cae18d
unknown <tylergobran@shaw.ca>
2018-11-04 23:47:09 -0700

    Began UI work

---
+++ app/src/main/java/cs/ualberta/ca/medlog/activity/activity_startScreen.java
@@ -0,0 +1,15 @@
+package cs.ualberta.ca.medlog.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs.ualberta.ca.medlog.R;
+
+public class activity_startScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
