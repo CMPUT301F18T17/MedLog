e458e406b1761dc8e6796bcf28069775c7ca99a5
chomyc1 <chomyc1@ualberta.ca>
2018-11-15 15:26:18 -0700

    Finished fixing merge stuff

--- app/src/main/java/cs/ualberta/ca/medlog/activity/PatientAddRecordActivity.java
+++ app/src/main/java/cs/ualberta/ca/medlog/activity/PatientAddRecordActivity.java
@@ -2,6 +2,7 @@ package cs.ualberta.ca.medlog.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +10,6 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.mapbox.mapboxsdk.geometry.LatLng;
 
 import java.util.ArrayList;
 
