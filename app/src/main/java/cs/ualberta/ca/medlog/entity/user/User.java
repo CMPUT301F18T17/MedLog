717460c131efaee438a04164c530cad8af7140ef
Thomas Roskewich <roskewic@ualberta.ca>
2018-11-14 19:17:46 -0700

    Added lastUpdated to User

--- app/src/main/java/cs/ualberta/ca/medlog/entity/user/User.java
+++ app/src/main/java/cs/ualberta/ca/medlog/entity/user/User.java
@@ -1,10 +1,13 @@
 package cs.ualberta.ca.medlog.entity.user;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
 
 public abstract class User implements Serializable {
 
     private String username;
+    private Date lastUpdated;
 
     public User(String username){
         this.username = username;
@@ -13,4 +16,5 @@ public abstract class User implements Serializable {
     public String getUsername() {
         return username;
     }
+    public void setUpdated(){ lastUpdated = Calendar.getInstance().getTime(); }
 }
