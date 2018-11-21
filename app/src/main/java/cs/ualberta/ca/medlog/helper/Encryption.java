424bda4f118e2b66176e08a1bc8d9049a33b8dc3
Thomas <thomas354@shaw.ca>
2018-11-21 16:41:56 -0700

    Documentation + bitmap encryption

--- app/src/main/java/cs/ualberta/ca/medlog/helper/Encryption.java
+++ app/src/main/java/cs/ualberta/ca/medlog/helper/Encryption.java
@@ -1,7 +1,12 @@
 package cs.ualberta.ca.medlog.helper;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Base64;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 import java.security.Key;
 import java.security.MessageDigest;
 import java.util.Arrays;
@@ -11,19 +16,43 @@ import javax.crypto.spec.SecretKeySpec;
 
 import cs.ualberta.ca.medlog.singleton.CurrentUser;
 
+/**
+ *
+ * <h1>
+ *     Encryption
+ * </h1>
+ *
+ *
+ *  <p>
+ *     Description: <br>
+ *         The purpose of this class is to allow for encryption of byte arrays based off the
+ *         users provided username (key).
+ *
+ * </p>
+ *
+ * <p>
+ *     References: <br>
+ *
+ *         https://stackoverflow.com/questions/7360403/base-64-encode-and-decode-example-code
+ *         https://stackoverflow.com/questions/3451670/java-aes-and-using-my-own-key
+ *         https://stackoverflow.com/questions/24910620/encode-and-decode-bitmap-to-byte-array-without-compress
+ *         Accessed Nov 20, 2018
+ * </p>
+ *
+ * @author Thomas Roskewic
+ * @contact roskewic@ualberta.ca
+ */
 public class Encryption {
-
-    /*
-    https://stackoverflow.com/questions/7360403/base-64-encode-and-decode-example-code
-    https://stackoverflow.com/questions/3451670/java-aes-and-using-my-own-key
-     */
 
     private static String algorithm = "AES";
 
     /**
-     * <p>Encrypt the provided data using AES</p>
+     * <p>Encrypt the data using AES</p>
+     * @param username The username (key).
+     * @param data The data to encrypt as a byte array.
+     * @return A string of the encrypted data.
      */
-    public static String encryptData(String username, String data){
+    public static String encryptData(String username, byte[] data){
         try {
 
             // Generate a key based off the username using SHA-1
@@ -38,15 +67,21 @@ public class Encryption {
             cipher.init(Cipher.ENCRYPT_MODE, key);
 
             // Get the encrypted string and return.
-            byte[] encoded = cipher.doFinal(data.getBytes());
+            byte[] encoded = cipher.doFinal(data);
             return Base64.encodeToString(encoded, Base64.DEFAULT);
         }catch(Exception e){
             e.printStackTrace();
         }
-        return data;
+        return new String(data);
     }
 
-    public static String decryptData(String username, String data){
+    /**
+     * <p>Decrypt the data provided using AES.</p>
+     * @param username The username (key).
+     * @param data The data to decrypt as a string.
+     * @return The byte array of the data decrypted.s
+     */
+    public static byte[] decryptData(String username, String data){
         try{
             // Generate a key based off the username using SHA-1
             byte[] keyV = username.getBytes("UTF-8");
@@ -61,11 +96,41 @@ public class Encryption {
 
             byte[] decodedVal = Base64.decode(data, Base64.DEFAULT);
             byte[] decVal = cipher.doFinal(decodedVal);
-            return new String(decVal);
+            return decVal;
 
         }catch(Exception e){
             e.printStackTrace();
         }
-        return data;
+        return data.getBytes();
+    }
+
+    /**
+     * <p>Converts a bitmap to its respective byte array.</p>
+     * @param b The bitmap to get the bytes for.
+     * @return The byte array for the bitmap.
+     */
+    public static byte[] bitMaptoBytes(Bitmap b){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(b.getByteCount());
+        b.copyPixelsToBuffer(byteBuffer);
+        byteBuffer.rewind();
+        return byteBuffer.array();
+    }
+
+    /**
+     * <p>Converts a byte array back into a Bitmap.</p>
+     * @param b The byte array.
+     * @return The bitmap from the byte array.
+     */
+    public static Bitmap byteArrayToBitmap(byte[] b){
+        return BitmapFactory.decodeByteArray(b, 0, b.length);
+    }
+
+    /**
+     * <p>Converts a byte array into a String.</p>
+     * @param b The byte array.
+     * @return A string of the byte array.
+     */
+    public static String byteArrayToString(byte[] b){
+        return new String(b);
     }
 }
--- app/src/test/java/cs/ualberta/ca/medlog/EncryptionTest.java
+++ app/src/test/java/cs/ualberta/ca/medlog/EncryptionTest.java
@@ -1,8 +1,14 @@
 package cs.ualberta.ca.medlog;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+
+import java.util.Arrays;
+import java.util.Random;
 
 import cs.ualberta.ca.medlog.helper.Encryption;
 
@@ -14,11 +20,36 @@ public class EncryptionTest {
 
     @Test
     public void testEncrypt(){
-        String enc = Encryption.encryptData("roskewic", "TEST MESSAGE");
+        String enc = Encryption.encryptData("roskewic", "TEST MESSAGE".getBytes());
         System.out.println(enc);
-        String dec = Encryption.decryptData("roskewic", enc);
+        byte[] dec = Encryption.decryptData("roskewic", enc);
         assertFalse("Messages are equal after encryption.", enc.equals("TEST MESSAGE"));
-        assertNotSame("The encrypted and decrypted strings are the same!", enc, dec);
-        assertEquals("Decrypted message does not equal initial message.", "TEST MESSAGE", dec);
+        assertNotSame("The encrypted and decrypted strings are the same!", enc, Encryption.byteArrayToString(dec));
+        assertEquals("Decrypted message does not equal initial message.", "TEST MESSAGE", Encryption.byteArrayToString(dec));
+    }
+
+    @Test
+    public void testBitmapEncrypt(){
+        Bitmap test = generateBitmap(100, 100);
+        String enc = Encryption.encryptData("roskewic", Encryption.bitMaptoBytes(test));
+        System.out.println(enc);
+        byte[] dec = Encryption.decryptData("roskewic", enc);
+        assertTrue("Decrypted data bytes are not equal.", Arrays.equals(dec, Encryption.bitMaptoBytes(test)));
+        Bitmap out = Encryption.byteArrayToBitmap(dec);
+        // The bitmap encryption is lossy, i.e. some data is lost in compression.
+        // Therefor, I am unable to test if the encryption is successful without a bitmap viewer.
+
+    }
+
+
+    private Bitmap generateBitmap(int width, int height){
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888, false);
+        Random r = new Random();
+        for(int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                bitmap.setPixel(j, i, Color.rgb(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+            }
+        }
+        return bitmap;
     }
 }
