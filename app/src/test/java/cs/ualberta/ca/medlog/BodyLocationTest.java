a04b81bcdb92c4d46396452805f3b755d68116a9
unknown <tylergobran@shaw.ca>
2018-12-01 18:41:46 -0700

    Updated bodylocation test documentation, removed example test

--- app/src/test/java/cs/ualberta/ca/medlog/BodyLocationTest.java
+++ app/src/test/java/cs/ualberta/ca/medlog/BodyLocationTest.java
@@ -1,11 +1,3 @@
-/*
- * Test class for the BodyLocation entity
- *
- * Authors: Thomas Roskewich, Tem Tamre
- * Contact: roskewic@ualberta.ca, ttamre@ualberta.ca
- * Created: October 30, 2018
- */
-
 package cs.ualberta.ca.medlog;
 
 import org.junit.Test;
@@ -14,8 +6,24 @@ import cs.ualberta.ca.medlog.entity.BodyLocation;
 import cs.ualberta.ca.medlog.entity.Photo;
 import static junit.framework.TestCase.assertEquals;
 
+/**
+ * <p>
+ *     Description: <br>
+ *         Test class for the BodyLocation entity.
+ * </p>
+ * <p>
+ *     Issues: <br>
+ *         None.
+ * </p>
+ *
+ * @author Thomas Roskewich, Tem Tamre
+ * @version 1.0
+ */
 public class BodyLocationTest {
 
+    /**
+     * Tests if a BodyLocations photo is successfully returned.
+     */
     @Test
     public void testGetPhoto(){
         Photo photo = new Photo(null);
@@ -24,14 +32,20 @@ public class BodyLocationTest {
         assertEquals(photo, location.getPhoto());
     }
 
+    /**
+     * Tests if a bodyLocations x percentage on the photo is successfully returned.
+     */
     @Test
     public void testGetX(){
         Photo photo = new Photo( null);
-        BodyLocation location = new BodyLocation(photo, 10, 5);
+        BodyLocation location = new BodyLocation(photo, 0.1f, 0.5f);
 
-        assertEquals((int)location.getX(), 10);
+        assertEquals((int)location.getX(), 0.1f);
     }
 
+    /**
+     * Tests if a bodyLocations y percentage on the photo is successfully returned.
+     */
     @Test
     public void testGetY(){
         Photo photo = new Photo(null);
